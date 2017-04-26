90d585bf634563a2764f9d7f7a5bf64ab7d78504
Supakorn Thongtra <rainstorm.armiie@gmail.com>
2017-04-26 09:22:03 +0700

    import smooth-scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild  } from '@angular/core';
+import Scrollbar from 'smooth-scrollbar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'app works!';
+
+  @ViewChild('scrolltest') scrolltest: any;
+
+  options = {
+    speed:1,
+  }
+
+  ngOnInit() {
+    console.log(Scrollbar);
+  }
+
+  ngAfterViewInit() {
+    let elem = this.scrolltest.nativeElement;
+    console.log(elem);
+    // Scrollbar.init(document.getElementById('my-scrollbar'), this.options);
+    // Scrollbar.init(elem, this.options);
+  }
 }
